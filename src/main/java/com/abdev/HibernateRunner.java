1736aedd1d6a6c02041adc633d16bd69389c4d25
ArmanBekmuratov <investakiosha@gmail.com>
2022-06-25 00:18:01 -0700

    usage of log4j file

--- src/main/java/com/abdev/HibernateRunner.java
+++ src/main/java/com/abdev/HibernateRunner.java
@@ -3,10 +3,16 @@ package com.abdev;
 import com.abdev.entity.User;
 import com.abdev.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 public class HibernateRunner {
+
+    private static final Logger log =  LoggerFactory.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) throws SQLException {
         User user = User.builder()
                 .username("john12@gmail.com")
@@ -14,23 +20,24 @@ public class HibernateRunner {
                 .lastname("Trump")
                 .build();
 
+        log.info("User entity is transient state, object: {}", user);
+
         try (var sessionFactory = HibernateUtil.buildSessionFactory()) {
-            try (Session session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+            Session session1 = sessionFactory.openSession();
+            try (session1) {
+
+                Transaction transaction = session1.beginTransaction();
+                log.info("Transaction is created: {}", transaction);
 
                 session1.saveOrUpdate(user);
+                log.info("User is in persistent state: {}, session {} ", user, session1);
 
                 session1.getTransaction().commit();
             }
-
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-
-
-                session2.getTransaction().commit();
-            }
-
+            log.warn("Session is in detached state: {}, session {} ", user, session1);
+        } catch (Exception e) {
+            log.error("Exception: ", e);
+            throw e;
         }
     }
 }
